a3d4b65dc9bb0b3b09b279f3005297a629f155c1
hasindusithmin <hasindusithmin64@gmail.com>
2022-04-23 19:11:00 +0530

    create spring boot project

---
+++ src/main/java/com/codeunity/uno/UnoApplication.java
@@ -0,0 +1,13 @@
+package com.codeunity.uno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoApplication.class, args);
+	}
+
+}
