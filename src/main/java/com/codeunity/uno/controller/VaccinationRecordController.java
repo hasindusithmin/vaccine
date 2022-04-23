570f2c72f3a3a2d3d21f9a4e11d9207d4f030537
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-23 16:51:14 +0000

    create new routes

--- src/main/java/com/codeunity/uno/controller/VaccinationRecordController.java
+++ src/main/java/com/codeunity/uno/controller/VaccinationRecordController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,17 @@ public class VaccinationRecordController {
     @GetMapping("/{id}")
     public ResponseEntity<VaccinationRecordEntity> getOne(@PathVariable int id) {
         return ResponseEntity.ok(vaccinationRecordService.read(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<VaccinationRecordEntity> updateOne(@RequestBody VaccinationRecordEntity vaccinationRecordEntity,@PathVariable int id){
+        return ResponseEntity.ok(vaccinationRecordService.update(vaccinationRecordEntity, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOne(@PathVariable int id){
+        vaccinationRecordService.detele(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/codeunity/uno/service/VaccinationRecordService.java
+++ src/main/java/com/codeunity/uno/service/VaccinationRecordService.java
@@ -2,6 +2,7 @@ package com.codeunity.uno.service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -27,5 +28,23 @@ public class VaccinationRecordService {
 
     public VaccinationRecordEntity read(int id){
         return vaccinationRecordRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public VaccinationRecordEntity update(VaccinationRecordEntity vaccinationRecordEntity,int id) {
+        VaccinationRecordEntity vaccination_record_entity = vaccinationRecordRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        vaccination_record_entity.setIdentificationNumber(vaccinationRecordEntity.getIdentificationNumber());
+        vaccination_record_entity.setFirstName(vaccinationRecordEntity.getFirstName());
+        vaccination_record_entity.setLastName(vaccinationRecordEntity.getLastName());
+        vaccination_record_entity.setAddress(vaccinationRecordEntity.getAddress());
+        vaccination_record_entity.setCity(vaccinationRecordEntity.getCity());
+        vaccination_record_entity.setSerialNumber(vaccinationRecordEntity.getSerialNumber());
+        vaccination_record_entity.setDose(vaccinationRecordEntity.getDose());
+        vaccination_record_entity.setBrand(vaccinationRecordEntity.getBrand());
+        return vaccinationRecordRepository.save(vaccination_record_entity);
+    }
+
+    public void detele(int id) {
+        VaccinationRecordEntity vaccinationRecordEntity = vaccinationRecordRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        vaccinationRecordRepository.delete(vaccinationRecordEntity);
     }
 }
