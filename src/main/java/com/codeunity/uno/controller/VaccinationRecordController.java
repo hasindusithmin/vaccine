36ddefcaa20fea4419a5f7e22d2a1f0e0d31abb9
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-23 17:03:04 +0000

    create index.html & cors disable

--- src/main/java/com/codeunity/uno/controller/VaccinationRecordController.java
+++ src/main/java/com/codeunity/uno/controller/VaccinationRecordController.java
@@ -8,7 +8,6 @@ import com.codeunity.uno.service.VaccinationRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/vaccination")
-@CrossOrigin
 public class VaccinationRecordController {
     
     @Autowired
