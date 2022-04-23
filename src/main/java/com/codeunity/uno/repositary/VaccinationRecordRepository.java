2e2c3fdd5835e342e68033d93d32c59aea0ed175
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-23 15:14:18 +0000

    create VaccinationRecordRepository

---
+++ src/main/java/com/codeunity/uno/repositary/VaccinationRecordRepository.java
@@ -0,0 +1,9 @@
+package com.codeunity.uno.repositary;
+
+import com.codeunity.uno.model.VaccinationRecordEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VaccinationRecordRepository extends JpaRepository<VaccinationRecordEntity,Long> {
+    
+}
